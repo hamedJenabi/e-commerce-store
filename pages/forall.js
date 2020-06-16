170dedb84d6e18d8c7f0716169520bbb556eb27f
Hamed <hamed.jenabi@gmail.com>
2020-06-16 13:43:09 +0200

    Edited ForAll page

--- pages/forall.js
+++ pages/forall.js
@@ -7,7 +7,6 @@ import Header from '../components/Header.tsx';
 import Header_2 from '../components/Header_2';
 import Footer from '../components/Footer';
 import CartButton from '../components/CartButton';
-import { useState } from 'react';
 
 export default function uniSex(props) {
   const [cart, setCart] = useState(props.cartList ?? []);
